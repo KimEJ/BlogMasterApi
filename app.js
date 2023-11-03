d5b4a10b520c70131ba0e96f80b87f6db7f9a776
ujkim <ujkim@interpass.kr>
2023-11-03 17:14:40 +0900

    init

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// CORS 설정
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+               "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/nouns.js
+++ routes/nouns.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mecab = require('mecab-ya');
+var tfidf = require('../utils/simple-tf-idf-test');
+
+// router.post('/', function(req, res, next) {
+//     mecab.nouns(req.body.text, function (err, result) {
+//         console.log(result);
+//         res.json(result);
+//     });
+// });
 
 router.post('/', function(req, res, next) {
-    mecab.nouns(req.body.text, function (err, result) {
-        res.json(result);
-    });
+    const { docs, title } = req.body
+    let test = tfidf.similarity_test([title, ...docs])
+
+    console.log(test)
+    res.json(test);
 });
 
 module.exports = router;
